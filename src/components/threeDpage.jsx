809d3c7e48add97563db9d8c7a975a1ebee75ff4
Agwa <agwa.dan@gmail.com>
2021-03-08 13:47:15 +0300

    Add interaction to 3d object

--- src/components/threeDpage.jsx
+++ src/components/threeDpage.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './threeDpage.css';
 
 import { Canvas } from 'react-three-fiber';
 
 const Box = () => {
+
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+
   return (
-    <mesh>
-      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-      <meshBasicMaterial attach='material' color='blue' />
+    <mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+    >
+      <boxBufferGeometry attach='geometry' args={[1.5, 1.5, 1.5]} />
+      <meshBasicMaterial attach='material' color={hovered ? 'blue' : 'gray'} />
     </mesh>
   )
 }
