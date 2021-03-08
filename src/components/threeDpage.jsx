bcc5ca4e7758d9eb23cdcabb6e195727312e9053
Agwa <agwa.dan@gmail.com>
2021-03-08 15:19:43 +0300

    Add rotation animation to box

--- src/components/threeDpage.jsx
+++ src/components/threeDpage.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './threeDpage.css';
 
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { useSpring, a } from 'react-spring/three'; //--- a is shortcut for animated.
 
 const Box = () => {
-
+  const meshRef = useRef();
   const [hovered, setHovered] = useState(false);
   const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? 'blue' : 'gray'
+  })
+
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01;
+  })
 
   return (
-    <mesh
+    <a.mesh
+      ref={meshRef}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      scale={props.scale}
     >
       <boxBufferGeometry attach='geometry' args={[1.5, 1.5, 1.5]} />
-      <meshBasicMaterial attach='material' color={hovered ? 'blue' : 'gray'} />
-    </mesh>
+      <a.meshBasicMaterial attach='material' color={props.color} />
+    </a.mesh>
   )
 }
 
