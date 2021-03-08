8eba30547f2c29cd11d74a9daa870328cd1f1ebd
Agwa <agwa.dan@gmail.com>
2021-03-08 13:29:17 +0300

    Render simple box

--- src/components/threeDpage.jsx
+++ src/components/threeDpage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './threeDpage.css';
 
 import { Canvas } from 'react-three-fiber';
+//import { BoxBufferGeometry } from 'three';
 
 const ThreeDpage = () => {
   return (
@@ -9,7 +10,10 @@ const ThreeDpage = () => {
       <h2>3D Page</h2>
       <Canvas>
         <mesh>
-
+          <boxBufferGeometry
+            attach='geometry'
+            arcs={[1, 1, 1]}
+          />
         </mesh>
       </Canvas>
     </div>
